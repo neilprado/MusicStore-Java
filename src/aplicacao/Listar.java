0dc38617457089897e7d5b2b85246c0777741691
neilprado <neiljohn17@gmail.com>
2018-10-05 13:51:20 -0300

    Método Listar criado

---
+++ src/aplicacao/Listar.java
@@ -0,0 +1,24 @@
+package aplicacao;
+
+import fachada.Fachada;
+
+public class Listar {
+	public Listar() {
+		Fachada.inicializar();
+		try {
+			System.out.println(Fachada.listarArtistas());
+			System.out.println(Fachada.listarAlbum());
+			System.out.println(Fachada.listarMusicasCadastradas());
+			System.out.println(Fachada.listarGeneros());
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		Fachada.finalizar();
+		System.out.println("Fim do Programa");
+	}
+
+	public static void main(String[] args) {
+		new Listar();
+	}
+
+}
